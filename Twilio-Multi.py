b1cd5b0ae4d95846c04bed558a9a692ed5e05840
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2012-07-12 00:21:38 +0000

    oopdates

--- Twilio-Multi.py
+++ Twilio-Multi.py
@@ -1,5 +1,15 @@
-from flask import Flask, request, redirect, session
+import sqlite3
 import twilio.twiml
+from contextlib import closing
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+# configuration
+DATABASE = 'multitude_data.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
  
 # The session object makes use of a secret key.
 SECRET_KEY = 'a secret key'
@@ -12,6 +22,14 @@ callers = {
     "+14158675310": "Boots",
     "+14158675311": "Virgil",
 }
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
@@ -34,11 +52,25 @@ def hello_monkey():
 
     message = "".join([name, " has messaged ", request.form['To'], " ", str(counter), " times."])
     #message = "".join([name, " has messaged ", str(counter)])
+
+    g.db.execute('insert into respondents (name, phone_no) values (?, ?)',
+                 [name, from_number])
+    g.db.commit()
+    print "Inserted name and no to sqlite3"
  
     resp = twilio.twiml.Response()
     resp.sms(message)
 
     return str(resp)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
  
 if __name__ == "__main__":
     app.run(debug=True)
