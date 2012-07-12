608496c8a8b5d5cc70d5634f05443d5fa67786f0
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2012-07-12 15:39:57 +0000

    Schema and python updates

---
+++ schema.sql
@@ -0,0 +1,29 @@
+drop table if exists answers;
+drop table if exists questions;
+drop table if exists respondents;
+drop table if exists surveys;
+
+create table answers (
+  id integer primary key autoincrement,
+  respondent_id integer not null,
+  question_id integer not null,
+  text string not null
+);
+
+create table questions (
+  id integer primary key autoincrement,
+  survey_id integer not null,
+  question_number integer not null,
+  text string not null
+);
+
+create table respondents (
+  id integer primary key autoincrement,
+  name string not null,
+  phone_no string not null
+);
+
+create table surveys (
+  id integer primary key autoincrement,
+  title string not null
+);
