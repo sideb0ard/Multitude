b18baba66d08d8fce3a2faecada4c6928310fe43
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2012-07-13 05:43:13 +0000

    Oopdates

--- Twilio-Multi.py
+++ Twilio-Multi.py
@@ -32,36 +32,75 @@ def teardown_request(exception):
     g.db.close()
  
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
-
-    counter = session.get('counter', 0)
-         
-    # increment the counter
-    counter += 1
-    # Save the new counter value in the session
-    session['counter'] = counter
+def mr_question():
+    """Main body - take SMS message, query/update db and respond"""
 
     from_number = request.form['From']
-    if from_number in callers:
-        name = callers[from_number]
+
+    cursor=g.db.cursor()
+    cursor.execute('SELECT id,phone_no,name FROM respondents WHERE phone_no=? LIMIT 1', [from_number])
+    respondent=cursor.fetchone()
+    if respondent is None:
+        state = session.get('state',0)
+        if state == 0: # INITIAL MESSAGE
+            session['state'] = 'register'
+            message = "Hola, please send me your name to continue"
+        elif state == 'register':
+            #  CHECK FOR MESSAGE AND USE AS NAME
+            # inset name
+            # GET QUESTION ONE
+            # RETURN QUESTION ONE
+            name = request.form['Body']
+            message = "hey ".join([name, ", QUESTION ONE"])
+            session['state'] = 1
+        else: # SOMETHING WRONG - DELETE ALL EVIDENCE, RETREAT!RETREAT!!
+            # DELETE ALL COOKIES
+            print "State follows.."
+            print ", ".join(state)
+            session['state'] = []
+            message = "something FUCKED UP! deleting all cookies.."
     else:
-        name = "Monkey"
+        #name = respondent[2]
+        name = respondent[2]
+        current_q = session.get('state',0)
+        # ERROR CHECK - GRAB LATEST QUESTION FOR THIS RESPONDENT FROM DB AND COMPARE WITH COOKIE
+        # GET REPLY - CHECK NOT NULL
+        # UPDATE ANSWERS WITH CURRENT_Q AND TEXT
+        # GET NEXT QUESTION CURRENT_Q += 1
+        current_q += 1
+        # Save the new counter value in the session
+        session['current_q'] = current_q
+
+        message = "hey ".join([name, " your're on question", str(current_q)])
+        print message
 
     to_number = request.form['To']
 
-    message = "".join([name, " has messaged ", request.form['To'], " ", str(counter), " times."])
     #message = "".join([name, " has messaged ", str(counter)])
 
-    g.db.execute('insert into respondents (name, phone_no) values (?, ?)',
-                 [name, from_number])
-    g.db.commit()
-    print "Inserted name and no to sqlite3"
+    #g.db.execute('insert into respondents (name, phone_no) values (?, ?)',
+    #             [name, from_number])
+    #g.db.commit()
+    #print "Inserted name and no to sqlite3"
  
     resp = twilio.twiml.Response()
     resp.sms(message)
 
     return str(resp)
+
+@app.route("/showquestions")
+def show_questions():
+    cur = g.db.execute('select id, survey_id, question_number, text from questions order by id desc')
+    questions = [dict(id=row[0], survey_id=row[1],question_number=row[2], text=row[3]) for row in cur.fetchall()]
+    return render_template('show_questions.html', questions=questions)
+
+@app.route('/addquestion', methods=['POST'])
+def add_question():
+    g.db.execute('insert into questions (survey_id, question_number, text) values (?, ?, ?)',
+                 [request.form['survey_id'], request.form['question_number'], request.form['text']])
+    g.db.commit()
+    flash('New question was successfully added')
+    return redirect(url_for('show_questions'))
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
